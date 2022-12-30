e0b785413ad7acb6551182fa3566f77a062c0a9f
Spectra <monctrik@yandex.ru>
2022-12-30 12:33:02 +0300

    second branch 3 commit

--- app/src/main/java/com/flycode/myapplication/MainActivity.kt
+++ app/src/main/java/com/flycode/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        println("second branch commit")
+        println("qewqeqweqeqweqwe")
 
         println()
     }
